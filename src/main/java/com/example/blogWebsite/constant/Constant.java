c5e8b3b3a6376f070b82540ae879e2c6ac9b58ff
doshi30 <doshi30@avaya.com>
2024-03-10 19:23:23 +0530

    Revert "Backend Logic"
    This reverts commit 6f38edec2ea93376ca128df09b941ffa6383f1b2.

--- src/main/java/com/example/blogWebsite/constant/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.example.blogWebsite.constant;
-
-public class Constant {
-
-    public static final String BLOG_INDEX_FORMAT = "blog_%s";
-
-    public static final String ELASTIC_INDEX_PREFIX_DATE_FORMAT = "MM-yyyy";
-}
